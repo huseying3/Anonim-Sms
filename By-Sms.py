70b868f04fbbaf28acfc07b8d1f21b1053d5b892
byh4ckerr <65884146+byh4ckerr@users.noreply.github.com>
2020-09-25 02:14:36 +0300

    Update By-Sms.py

--- By-Sms.py
+++ By-Sms.py
@@ -1,32 +1,50 @@
-#python 3.7.1
-print(ByH4cker Sms Gönderme Tool)
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 import requests
+from os import system as s
 
-while True:
-  number = input("Sms atacağınız kişinin numarasını bunla birlikte yazın '+':")
-  message = input('Atacağınız mesajınız: ')
+import os
 
-  print(f'Receiver : {number}\nYour Message: {message}')
+os.system("apt-get install figlet")
 
-  print("Devam etmek için (1)\n Fikrinizi Değiştirmek için (2)")
-  edit=int(input())
-  
-  if edit==1:
+os.system("clear")
+
+os.system("figlet By H4cker - SMS GÖNDERME ARACI")
+
+banner = """
+         	          >Coder By H4cker<
+|> İstediğiniz telefon adresine hergün 1 defa mesaj atma hakkınız vardır!
+|> Mesajınızdaki karakter sayısı sınırlıdır.
+|> Telefon numarasını doğru girmezseniz hata vericektir.
+|> Çalıştığını kendinizde deneyebilirsiniz.
+|> Instagram = byh4cker 
+|> YouTube = By H4cker
+|> Telefon = +1 (819) 800-8547
+"""
+
+print(banner)
+
+sor = input("Telefon Numarası Örn:+905555555555 >>> ")
+
+mesaj = input("Mesajınız >>> ")
+
+arlk = mesaj[0:70]
+
+print("\n| Mesajınızın gönderilebilecek kısmı aşağıdaki gibidir.\n"+arlk)
+
+drlm = input("\n| Mesajınız gönderilsin mi?[y/n] >>> ")
+
+if drlm == "y" or drlm == "Y":
+    print("\n"+sor+"\n"+arlk+"\n")
     resp = requests.post('https://textbelt.com/text', {
-    'phone': number,
-    'message': message,
-    'key': 'textbelt',
+  'phone': sor,
+  'message': arlk,
+  'key': 'textbelt',
     })
-    response = resp.json()
-    if response['success'] == False:
-       print('ERROR : ',response['error'])
+    print(resp.json())
 
-    elif response['success'] == True:
-       print('DONE! Your Message ID is:',response['textId'])
-    break
-  elif edit==2:
-    continue
-  else:
-     print("Wrong Command!\nBe sure you to check the message info for security")
-  continue
-  
+elif drlm == "n" or drlm == "N":
+    quit()
+else:
+    print("\n|Hata yaptınız.")
